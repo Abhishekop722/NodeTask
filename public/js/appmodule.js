c2fba8acea4d6fecbe6cb5d51306d527f0527f03
Abhishek Sharma <abhishekop722@gmail.com>
2019-05-03 09:32:03 +0530

    Update appmodule.js

--- public/js/appmodule.js
+++ public/js/appmodule.js
@@ -1,7 +1,8 @@
 const appModule = angular.module('app',['ngRoute']);
 appModule.constant('SUBMIT_URL','http://localhost:1234/submit');
 appModule.constant('HISTORY_URL','http://localhost:1234/history');
-appModule.config(['$routeProvider','$locationProvider',($routeProvider,$locationProvider)=>{
+appModule.config(['$routeProvider','$locationProvider','$httpProvider',($routeProvider,$locationProvider,$httpProvider)=>{
+    $httpProvider.defaults.timeout = 80000;
     $locationProvider.hashPrefix('');
     $routeProvider.when('/',{
         templateUrl:'../views/home.html'
